e82d0599416241253ec7eb819ee9d808770c93a0
Orizune <50743974+Orizune@users.noreply.github.com>
2024-08-29 08:49:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Add any interactive elements here
-document.querySelector('.btn').addEventListener('click', function() {
-    alert('Welcome to Acadpower Gym!');
-});
+//document.querySelector('.btn').addEventListener('click', function() {
+//    alert('Welcome to Acadpower Gym!');
+//});
